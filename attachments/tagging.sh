f5b37f795edff8c0c49235a0fbb1020ed5c3b3f7
Adam Alexander <adam.alexander@rightscale.com>
2016-04-25 12:13:54 -0400

    Update tagging logic

--- attachments/tagging.sh
+++ attachments/tagging.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 
-function tag_and_verify()
-{
-  local  tag=$1
-  echo "Adding server tag: $tag"
+tag_and_verify() {
+  while true; do
+    rsc --rl10 cm15 multi_add /api/tags/multi_add "resource_hrefs[]=$RS_SELF_HREF" "tags[]=$1"
 
-  rs_api_cmd="rsc"
+    tag=$(rsc --rl10 --xm ".name:val(\"$1\")" cm15 by_resource /api/tags/by_resource "resource_hrefs[]=$RS_SELF_HREF")
 
-  i="0"
-  while [ $i -lt 3 ]
-  do
-
-    # Use the RL10 proxy to access the api
-    echo "ADD TAG COMMAND: ${rs_api_cmd} --rl10 cm15 multi_add /api/tags/multi_add \"resource_hrefs[]=$RS_SELF_HREF\" \"tags[]=$tag\""
-    ${rs_api_cmd} --rl10 cm15 multi_add /api/tags/multi_add "resource_hrefs[]=$RS_SELF_HREF" "tags[]=$tag"
-
-    sleep 3
-    current_tag=$(${rs_api_cmd} --pp --rl10 cm15 by_resource /api/tags/by_resource "resource_hrefs[]=$RS_SELF_HREF" | grep "$tag" || true)
-
-    if test "$current_tag" = "" ; then
-      echo "[$i] Failed to add tag. Sleep and retry..."
+    if [[ "$tag" = "" ]]; then
+      sleep 1
     else
-      echo "[$i] Successfully added tag."
       break
     fi
-
-   sleep 5
-   i=$((i+1))
  done
 }
